27b05a8018861310970c83b3dc599d8752d51497
COOLBEANS\Jonathan <jonathan.widdowson1@gmail.com>
2019-07-31 15:12:25 +0300

    Add IsSearchObject(), allow URLs to be passed to navigate()
    Users can now navigate() pages using either URL strings or objects in the collection returned by  companion module search()

--- navigate.ps1
+++ navigate.ps1
@@ -8,6 +8,14 @@ function Navigate{
 
 	$navigationURL = RemoveIndex $searchURL
 	DisplayBodyInnerText $navigationURL
+	
+	if (IsSearchObject $searchURL){
+		$navigationURL = RemoveIndex $searchURL
+	}
+	else {
+		$navigationURL = $searchURL
+	}
+	$page = (Invoke-WebRequest $navigationURL)
 }
 
 # Remove URL's index position in result collection for navigation
@@ -32,4 +40,17 @@ function DisplayBodyInnerText{
 		[string]$searchURL
 	)
 	(Invoke-WebRequest $searchURL).ParsedHTML.body.innerText
+# Return true/false if provided URL for navigate() is/n't an object from search() array collection
+function IsSearchObject{
+	Param(
+		[Parameter(Position=0,
+		  Mandatory=$True,
+		  ValueFromPipeline=$False)]
+		[string]$navigationURL
+	)
+	if ($navigationURL[0] -eq "["){
+		return $true
+	}
+
+	return $false
 }
